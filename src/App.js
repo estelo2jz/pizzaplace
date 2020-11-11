1f2d49fb1c10f28b171df5a66c1c3059ebd925a9
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-11-11 07:04:39 -0600

    added multuple style components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
+    </Router>
   );
 }
 
